5453034be8031753220af8ad86f254c372d4e489
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-02-22 08:37:12 -0400

    Modularizated gitignores

--- qtile/.config/qtile/settings/themes.py
+++ qtile/.config/qtile/settings/themes.py
@@ -2,6 +2,7 @@ from os import path
 import json
 from settings.path import qtile_path
 
+
 def load_theme(theme="default"):
     theme_file = path.join(qtile_path, "themes", f"{theme}.json")
     if not path.isfile(theme_file):
@@ -11,6 +12,7 @@ def load_theme(theme="default"):
     with open(theme_file, mode="r", encoding="utf-8") as f:
         return json.load(f)
 
+
 def _create_default_config(config_file_path):
     with open(config_file_path, mode="w", encoding="utf-8") as f:
         json.dump({"theme": "default"}, f)
@@ -19,7 +21,7 @@ def _create_default_config(config_file_path):
 config_file_path = path.join(qtile_path, "settings.json")
 if not path.isfile(config_file_path):
     _create_default_config(config_file_path)
-with open(config_file_path, mode="r", encoding="utf-8") as f:
-    theme = json.load(f)['theme']
-    colors = load_theme(theme)
 
+with open(config_file_path, mode="r", encoding="utf-8") as f:
+    theme = json.load(f)["theme"]
+    colors = load_theme(theme)
