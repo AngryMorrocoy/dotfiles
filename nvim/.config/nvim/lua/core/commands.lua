b057842b18074e67f671991e40ecb0d845aa5128
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-07-12 22:56:19 -0400

    Fixed a typo with a custom command

--- nvim/.config/nvim/lua/core/commands.lua
+++ nvim/.config/nvim/lua/core/commands.lua
@@ -2,7 +2,7 @@ return {
     setup = function()
         vim.api.nvim_create_user_command("CopyFilePath", ":!echo '%:p' | xclip -i -sel c", {})
 
-        vim.api.nvim_create_user_command("T", "T split | resize 12 | terminal", {})
+        vim.api.nvim_create_user_command("T", "split | resize 12 | terminal", {})
         vim.api.nvim_create_user_command("Vt", "vertical split | vertical resize 80 | terminal", {})
 
         vim.api.nvim_create_user_command(
--- qtile/.config/qtile/settings/layouts.py
+++ qtile/.config/qtile/settings/layouts.py
@@ -3,8 +3,8 @@ from libqtile.config import Match
 from settings.themes import colors
 
 layout_theme = {
-    "border_width": 1,
-    "margin": 1,
+    "border_width": 2,
+    "margin": 8,
     "border_focus": colors["focused"][0],
     "border_normal": colors["unfocused"][0],
 }
@@ -13,7 +13,7 @@ layouts = [
     layout.Max(**layout_theme),
     layout.MonadTall(**layout_theme),
     layout.MonadWide(**layout_theme),
-    # layout.Stack(**layout_theme, num_stacks=2, autosplit=True),
+    layout.Stack(**layout_theme, num_stacks=2),
     layout.Matrix(
         **{key: value for key, value in layout_theme.items() if key != "margin"},
         margin=0
