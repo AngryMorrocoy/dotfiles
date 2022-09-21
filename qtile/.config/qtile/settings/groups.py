8e4101ed04b4ea3d12bd0b52763d208b65da1dbd
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-09-21 07:27:45 -0400

    Readding toggle when moving between groups

--- qtile/.config/qtile/settings/groups.py
+++ qtile/.config/qtile/settings/groups.py
@@ -18,7 +18,7 @@ groups = [Group(ws, **kwargs) for ws, kwargs in group_settings]
 for i, ws in enumerate(groups, 1):
     keys.extend([
         # Switch to a group with mod+number of that group
-        Key([mod], str(i), lazy.group[ws.name].toscreen()),
+        Key([mod], str(i), lazy.group[ws.name].toscreen(toggle=True)),
         # Move a window to a workspace with mod+shift+number of that group
         Key([mod, "shift"], str(i), lazy.window.togroup(ws.name)),
     ])
