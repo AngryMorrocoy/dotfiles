9b5f4bd0a977870b7445e0d5cdee9a540b8399a0
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-10-01 07:19:04 -0400

    ~ Ignore preference for spaces if stylua.toml exists

--- nvim/.config/nvim/lua/plugins/config/null_ls/stylua_formatter.lua
+++ nvim/.config/nvim/lua/plugins/config/null_ls/stylua_formatter.lua
@@ -1,9 +1,21 @@
 local null_ls = require("null-ls")
 
+local function stylua_config_exists()
+    local cwd = vim.fn.getcwd()
+    local stylua_config_file = io.open(cwd .. "/stylua.toml", "r")
+
+    return stylua_config_file ~= nil
+end
+
 return function()
-    local stylua_config = null_ls.builtins.formatting.stylua.with({
+    local config = null_ls.builtins.formatting.stylua.with({
         extra_args = { "--indent-type", "spaces" },
     })
 
-    null_ls.register(stylua_config)
+    -- Ignore my preference for spaces instead of tabs if stylua.toml exists
+    if stylua_config_exists() then
+        config = null_ls.builtins.formatting.stylua
+    end
+
+    null_ls.register(config)
 end
