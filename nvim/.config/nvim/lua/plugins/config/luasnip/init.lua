784841fb1ca550655c6a2dd8a515561e6f3340d2
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-10-21 07:08:01 -0400

    Remove unused function

--- nvim/.config/nvim/lua/plugins/config/luasnip/init.lua
+++ nvim/.config/nvim/lua/plugins/config/luasnip/init.lua
@@ -1,26 +1,5 @@
 local M = {}
 
-function M.load_snippets()
-    local ls = require("luasnip")
-    local concat_tables = require("core.util").concat_tables
-
-    local snippets = {
-        -- typescript = concat_tables({
-        --     require("plugins.config.luasnip.ts_snips").setup(),
-        --     require("plugins.config.luasnip.js_snips").setup(),
-        -- }),
-        -- typescriptreact = concat_tables({
-        --     require("plugins.config.luasnip.js_snips").setup(),
-        --     require("plugins.config.luasnip.tsreact_snips").setup(),
-        --     require("plugins.config.luasnip.ts_snips").setup(),
-        -- }),
-    }
-
-    for ft, snips in pairs(snippets) do
-        ls.add_snippets(ft, snips)
-    end
-end
-
 function M.setup()
     local ls = require("luasnip")
     local types = require("luasnip.util.types")
