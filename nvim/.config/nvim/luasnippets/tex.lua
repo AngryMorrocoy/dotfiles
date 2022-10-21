cd3add75536caa0c991073134fdd8c26904e4fe9
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-10-21 09:08:31 -0400

    Improving latex snippets

--- nvim/.config/nvim/luasnippets/tex.lua
+++ nvim/.config/nvim/luasnippets/tex.lua
@@ -1,4 +1,26 @@
-local manual_snips = {}
+local manual_snips = {
+    s({ trig = "[%d%a]*[%d%a]+/[%d%a]+[%d%a]*", regTrig = true }, {
+        f(function(_, parent)
+            local fraction = parent.trigger
+            local bar_index = fraction:find("/")
+
+            local numerator = string.sub(fraction, 1, bar_index - 1)
+            local denominator = string.sub(fraction, bar_index + 1)
+
+            return string.format("\\frac{%s}{%s}", numerator, denominator)
+        end, {}),
+    }, {}),
+    s(
+        "sq",
+        fmt("\\sqrt<>{<>}", {
+            c(1, {
+                t(""),
+                sn(nil, { t("["), i(1), t("]") }),
+            }),
+            i(0),
+        }, { delimiters = "<>" })
+    ),
+}
 local auto_snips = {
     s({ trig = "$", wordTrig = false }, {
         t("$"),
@@ -19,6 +41,13 @@ local auto_snips = {
             local last_char = string.sub(line_to_cursor, #line_to_cursor - 1, #line_to_cursor - 1)
             return last_char ~= "$"
         end,
+    }),
+    s({ trig = "[%d%a]_", regTrig = true, wordTrig = false }, {
+        d(1, function(_, parent)
+            local trigger = parent.trigger
+            -- %d_{${1}} ${0}
+            return sn(nil, { t(trigger), t("{"), i(1), t("} "), i(0) })
+        end, {}),
     }),
 }
 
